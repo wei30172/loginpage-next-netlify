9971b241b7d1dc6b8ff4c7d12a719eef685aeb00
wei30172 <claire610323@gmail.com>
2021-11-17 17:17:27 -0800

    initial

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div className="container">
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
+        <Image src="/rupee.png" width={50} height={50} />
+        <h1>Toy Vibes</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
